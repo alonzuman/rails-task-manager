66cdc9c691ba7ae74f27ec3286d3b024e71d0188
Alon Zuman <alonzuman7@gmail.com>
2020-03-16 13:32:11 +0200

    finished all just need to fix the whole mark as completed form input

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,46 @@
 class TasksController < ApplicationController
+  before_action :set_task, only:[:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(set_params)
+    @task.save
+
+    redirect_to tasks_path
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(set_params)
+
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task.destroy
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
     @task = Task.find(params[:id])
+  end
+
+  def set_params
+    params.require(:task).permit(:title, :details, :coompleted)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,18 @@ Rails.application.routes.draw do
   # Root
   root "tasks#index"
 
+  # Create
+  get "tasks/new", to: "tasks#new", as:"new_task"
+  post "tasks", to: "tasks#create"
+
   # Read
   get "tasks", to: "tasks#index"
   get "tasks/:id", to: "tasks#show", as: "task"
 
-  # Create
-
   # Update
+  get "tasks/:id/edit", to: "tasks#edit", as: "edit_task"
+  patch "tasks/:id", to: "tasks#update"
 
   # Delete
+  delete "tasks/:id", to: "tasks#destroy", as: "delete_task"
 end
