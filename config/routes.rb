d355ea96acbd7d61141681325ce1f2b97c39c2c0
Alon Zuman <alonzuman7@gmail.com>
2020-03-16 12:24:31 +0200

    Added routes and 'read' methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Root
+  root "tasks#index"
+
+  # Read
+  get "tasks", to: "tasks#index"
+  get "tasks/:id", to: "tasks#show", as: "task"
+
+  # Create
+
+  # Update
+
+  # Delete
 end
